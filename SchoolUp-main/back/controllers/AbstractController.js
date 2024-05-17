ac0ceace378d75f81bff207cff53a35c38beeb51
crentyalius <ignatevvd@gmail.com>
2024-05-17 12:50:16 +0700

    добавлена таблица факультета

--- SchoolUp-main/back/controllers/AbstractController.js
+++ SchoolUp-main/back/controllers/AbstractController.js
@@ -1,27 +1,26 @@
 const ApiError = require('../errors/ApiError');
+const {Faculty} = require('../models/model');
+
 
 class BaseController {
     constructor(model) {
-        this.model = model;
+        this.modell = model;
     }
-
+    //model=Faculty
     async create(req, res, next) {
-        try {
-            const { ...data } = req.body;
-            if (Object.keys(data).length === 0) {
-                throw ApiError.badRequest('Отсутствуют данные для создания сущности');
-            }
-            const entity = await this.model.create(data);
+        //try {
+            const data = req.body;
+            const entity = await this.modell.create(data);
             res.json(entity);
-        } catch (error) {
-            next(error);
-        }
+       // } catch (error) {
+       //     next(error); // Передаем ошибку обработчику ошибок
+       // }
     }
 
     async getAll(req, res, next) {
         try {
-            const entities = await this.model.findAndCountAll();
-            res.json(entities);
+            const entities = await this.modell.findAndCountAll();
+            return res.json(entities);
         } catch (error) {
             next(error);
         }
@@ -31,13 +30,13 @@ class BaseController {
         try {
             const { id, name } = req.body;
             if (name) {
-                await this.model.destroy({ where: { name } });
+                await this.modell.destroy({ where: { name } });
             } else if (id) {
-                await this.model.destroy({ where: { id } });
+                await this.modell.destroy({ where: { id } });
             } else {
                 throw ApiError.badRequest('Некорректные данные');
             }
-            res.json({ message: 'Удаление успешно' });
+            return res.json({ message: 'Удаление успешно' });
         } catch (error) {
             next(error);
         }
--- SchoolUp-main/back/controllers/FacultyController.js
+++ SchoolUp-main/back/controllers/FacultyController.js
@@ -1,11 +1,44 @@
-const BaseController = require('./BaseController');
+const BaseController = require('./AbstractController');
 const { Faculty } = require('../models/model'); // Подставьте нужную модель
-
+const modell =Faculty;
 class FacultyController extends BaseController {
-    constructor() {
-        super(Faculty); // Передаем модель Faculty в конструктор базового класса
+   
+    
+    //model=Faculty
+    async create(req, res, next) {
+        try {
+            const data = req.body;
+            const entity = await modell.create(data);
+            res.json(entity);
+        } catch (error) {
+            next(error); // Передаем ошибку обработчику ошибок
+        }
     }
 
+    async getAll(req, res, next) {
+        try {
+            const entities = await modell.findAndCountAll();
+            return res.json(entities);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async delete(req, res, next) {
+        try {
+            const { id, name } = req.body;
+            if (name) {
+                await modell.destroy({ where: { name } });
+            } else if (id) {
+                await modell.destroy({ where: { id } });
+            } else {
+                throw ApiError.badRequest('Некорректные данные');
+            }
+            return res.json({ message: 'Удаление успешно' });
+        } catch (error) {
+            next(error);
+        }
+    }
     // Дополнительные методы, специфичные для контроллера факультетов, можно добавить здесь
     // Например, методы для получения факультетов определенного университета и т. д.
 }
